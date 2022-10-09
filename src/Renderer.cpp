5a084d116718c39c8944d55b37534c504fc73d18
Christian <christian.stippel@gmail.com>
2022-10-09 22:50:31 +0200

    merge

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -168,11 +168,11 @@ void Renderer::render()
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // ourShader->use();
-    // auto viewMatrix = Window::getCamera()->getViewMatrix();
-    // ourShader->setMat4("view", viewMatrix);
-    // glm::vec3 viewPos = Window::getCamera()->getPosition();
-    // ourShader->setVec3("viewPos", viewPos);
+    ourShader->use();
+    auto viewMatrix = Window::getCamera()->getViewMatrix();
+    ourShader->setMat4("view", viewMatrix);
+    glm::vec3 viewPos = Window::getCamera()->getPosition();
+    ourShader->setVec3("viewPos", viewPos);
 
     // for (unsigned int i = 0; i < cubes.size(); i++)
     // {
